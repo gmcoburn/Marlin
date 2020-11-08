a3adbb9466c6e5129a2e6088554c6f7bd147926b
gmcoburn <74117682+gmcoburn@users.noreply.github.com>
2020-11-08 13:59:49 +1000

    UPadted aithor and confg directories

--- config/Configuration_adv.h
+++ config/Configuration_adv.h
@@ -21,7 +21,7 @@
  */
 #pragma once
 
-#define CONFIG_EXAMPLES_DIR "Creality/Ender-3/CrealityV1"
+#define CONFIG_EXAMPLES_DIR "Creality/Ender-3/MKS GEN L with TMC2208 and BLTouch"
 
 /**
  * Configuration_adv.h
